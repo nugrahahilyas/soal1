bb4b4e47a4954519162c0c2da94af4ae5c65387c
nugrahahilyas <nugraha.hilyas@gmail.com>
2023-08-29 07:54:43 +0700

    mengubah susunan file php dan html

--- 1a.php
+++ 1a.php
@@ -1,5 +1,13 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Soal 1a</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  </head>
+  <body>
 <?php 
-
 function timeInWords($h, $m){
     $huruf = [
         1 => "one",
@@ -61,15 +69,6 @@ fwrite($fptr, $result . "\n");
 fclose($fptr);
 
 ?>
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Soal 1a</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-  </head>
-  <body>
     <h1>Time To Word</h1>
     <?php if (isset($_POST)) : ?>
         <?php
